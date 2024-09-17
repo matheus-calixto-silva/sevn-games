ab9ac398a8d7f03fd14e55eddb90702a2c610ad1
Matheus Calixto <matheuscalixto8@gmail.com>
2024-09-17 20:50:47 -0300

    feat: criação da interface IMatch

---
+++ src/interfaces/IMatch.ts
@@ -0,0 +1,8 @@
+export interface IMatch {
+  team_home_id: string;
+  team_home_name: string;
+  team_home_score: number;
+  team_away_id: string;
+  team_away_name: string;
+  team_away_score: number;
+}
