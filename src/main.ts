ad6276acbb78fd9e8a05c97619c51ece18e8c93f
Matheus Calixto <matheuscalixto8@gmail.com>
2024-09-17 21:08:37 -0300

    feat: exibição das partidas e lógica de exibir diferentes rodadas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,115 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { SvgIcon } from './components/SvgIcon.ts';
+import type { IRound } from './interfaces/IRound.ts';
+import { getApiData } from './services/api.ts';
+import './style.css';
+
+let currentRound = 0;
+let apiRounds: IRound[] = [];
+
+const svgColors = [
+  '#FF0000', // Vermelho
+  '#00FF00', // Verde
+  '#0000FF', // Azul
+  '#FFFF00', // Amarelo
+  '#FF00FF', // Magenta
+  '#00FFFF', // Ciano
+  '#FFA500', // Laranja
+  '#800080', // Roxo
+  '#A52A2A', // Marrom
+  '#808080', // Cinza
+];
+
+let colorIndex = 0;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+<section>
+  <header class="header">
+    <h1>SEVN ESPORTES ⚽</h1>
+  </header>
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  <main class="content">
+    <h2>Confira as rodadas do nosso campeonato fictício!</h2>
+
+    <div class="match-card">
+
+      <div class="matches" id="matches">
+      <div class="matches-header">
+        <button class="nav-button" id="prev">&lt;</button>
+        <div>
+          <h3>Rodadas de Jogos</h3>
+          <h4 id="round">RODADA ${currentRound}</h4>
+        </div>
+      <button class="nav-button" id="next">&gt;</button>
+      </div>
+        <div id="match-list">
+        </div>
+    </div>
+  </main>
+</section>
+`;
+
+function updateSvgColor() {
+  const svgPaths = document.querySelectorAll('svg path');
+
+  svgPaths.forEach((path, index) => {
+    path.setAttribute(
+      'fill',
+      svgColors[(colorIndex + index) % svgColors.length]
+    );
+  });
+}
+
+function renderMatches(roundIndex: number) {
+  const roundData = apiRounds[roundIndex];
+
+  document.getElementById('round')!.innerText = `RODADA ${roundData.round}`;
+
+  const matchListHTML = roundData.games
+    .map(
+      (match) => `
+    <div class="match">
+      <div class="team left">
+        ${SvgIcon(svgColors[colorIndex])}
+        <span>${match.team_home_name}</span>
+      </div>
+      <div class="score">
+        <span>${match.team_home_score}</span> x <span>${
+        match.team_away_score
+      }</span>
+      </div>
+      <div class="team right">
+      <span>${match.team_away_name}</span>
+        ${SvgIcon(svgColors[colorIndex])}
+      </div>
+    </div>
+  `
+    )
+    .join('');
+
+  document.getElementById('match-list')!.innerHTML = matchListHTML;
+
+  updateSvgColor();
+
+  colorIndex = (colorIndex + 1) % svgColors.length;
+}
+
+document.getElementById('next')!.addEventListener('click', () => {
+  if (currentRound < apiRounds.length - 1) {
+    currentRound++;
+    renderMatches(currentRound);
+  }
+});
+
+document.getElementById('prev')!.addEventListener('click', () => {
+  if (currentRound > 0) {
+    currentRound--;
+    renderMatches(currentRound);
+  }
+});
+
+async function loadMatches() {
+  apiRounds = await getApiData();
+  renderMatches(currentRound);
+}
+
+loadMatches();
